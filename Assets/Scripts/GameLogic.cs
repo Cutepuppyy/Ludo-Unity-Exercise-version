dbd15eb5025edaa74d94d95e3fcbfebbd70ae932
Cutepuppyy <140346625+Cutepuppyy@users.noreply.github.com>
2025-02-19 08:50:33 +0100

    Making Scripts
    Added to the 3 existing scripts a Dice, a Player, and a Piece

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -5,3 +5,17 @@ public class GameLogic : MonoBehaviour
 {
         
 }
+//Making a dice
+        public class Dice
+        {
+            Random dice;
+            public Dice()
+            {
+                dice = new Random();
+            }
+            //The function to roll the dice
+            public int DiceSpacesToMove()
+            {
+                return dice.Next(1,7);
+            }
+        }
--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
+using static GameLogic;
+using static Player;
 
 public class GamePiece : MonoBehaviour
 {
+    //Making the variables the piece uses
+    public int SpacesMoved = 0;
+    public bool Home = true;
+    public bool Moving = false;
+    public bool Goal = false;
+    //A Constructor for the Piece
+    public GamePiece()
+    { }
+    public int SpacesMoving(int SpacesToMove)
+    {
+        return SpacesMoved = SpacesMoved + SpacesToMove;
+    }
 
-    
 }
