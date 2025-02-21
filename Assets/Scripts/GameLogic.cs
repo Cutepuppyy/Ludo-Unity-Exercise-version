1256c4e3f7d498f466e8dffe6c3d30578d4ee9c6
Cutepuppyy <140346625+Cutepuppyy@users.noreply.github.com>
2025-02-21 12:36:39 +0100

    Trying to add players

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,9 +1,24 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class GameLogic : MonoBehaviour
 {
-        
+    void OpretSpillere()
+    {
+        Player[] players = new Player[4];
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i] = new Player();
+        }
+    }
+    private void Start()
+    {
+        OpretSpillere();
+        int winnerFound = -1;
+    }
 }
 //Making a dice
 public class DiceRoller
@@ -15,3 +30,4 @@ public class DiceRoller
         Debug.Log("You rolled a " + diceRoll);
     }
 }
+
